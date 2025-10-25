9167bb62421733aaeb0551322c2be14a0136a692
James Guy <admin@Jamess-MacBook-Pro.local>
2025-10-25 12:57:38 -0500

    Replace webpack config with new block.json reference

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const path = require('path');
 module.exports = {
 	...defaultConfig,
 	entry: {
+		// Single blocks bundle - auto-imports all blocks
+		'blocks': path.resolve(process.cwd(), 'src/blocks.js'),
 		// Styles
 		'styles/tailwind': path.resolve(process.cwd(), 'src/styles/tailwind.css'),
 		'styles/editor': path.resolve(process.cwd(), 'src/styles/editor.scss'),
