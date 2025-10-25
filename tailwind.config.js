6d0d4ac400582ccfd27444be5a63361828069895
James Guy <admin@Jamess-MacBook-Pro.local>
2025-10-25 11:24:01 -0500

    Configure Tailwind - Create tailwind.config.js and postcss.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx}',
+    './templates/**/*.html',
+    './parts/**/*.html',
+    './patterns/**/*.php',
+    './*.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
