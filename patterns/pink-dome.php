c5b362bc1efbdbd026828a99a34e6885d03f6166
Your Name <you@example.com>
2026-05-26 11:42:45 -0500

    Polish patterns for launch

--- patterns/pink-dome.php
+++ patterns/pink-dome.php
@@ -8,10 +8,21 @@
  */
 ?>
 
+<?php
+/**
+ * Title: Pink Dome
+ * Slug: bloktastik/pink-dome
+ * Categories: lowercolumbia
+ * Description: A pink curve design to go above sections with a pink background
+ * Keywords: curve, dome, design
+ */
+?>
+
 <!-- wp:group {"metadata":{"categories":["lowercolumbia"],"patternName":"bloktastik/pink-dome","name":"Pink Dome"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"bottom":"var:preset|spacing|40","top":"0"}}},"backgroundColor":"blush","layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull has-blush-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:cover {"url":"http://lowercolumbia.test/wp-content/uploads/2026/02/header-dome.svg","id":357,"dimRatio":0,"overlayColor":"primary","isUserOverlayColor":false,"focalPoint":{"x":0.5,"y":0.84},"minHeight":65,"sizeSlug":"large","align":"full","className":"blush-dome","style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-cover alignfull blush-dome" style="margin-top:0;min-height:65px"><img class="wp-block-cover__image-background wp-image-357 size-large" alt="" src="http://lowercolumbia.test/wp-content/uploads/2026/02/header-dome.svg" style="object-position:50% 84%" data-object-fit="cover" data-object-position="50% 84%"/><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+<div class="wp-block-group alignfull has-blush-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:cover {"url":"/wp-content/uploads/2026/02/header-dome.svg","id":357,"dimRatio":0,"overlayColor":"primary","isUserOverlayColor":false,"focalPoint":{"x":0.5,"y":0.84},"minHeight":65,"sizeSlug":"large","align":"full","className":"blush-dome","style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignfull blush-dome" style="margin-top:0;min-height:65px"><img class="wp-block-cover__image-background wp-image-357 size-large" alt="" src="/wp-content/uploads/2026/02/header-dome.svg" style="object-position:50% 84%" data-object-fit="cover" data-object-position="50% 84%"/><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
 <p class="has-text-align-center has-large-font-size"></p>
 <!-- /wp:paragraph --></div></div>
 <!-- /wp:cover --></div>
 <!-- /wp:group -->
+ 
--- patterns/single-job-starter.php
+++ patterns/single-job-starter.php
@@ -15,14 +15,8 @@
 <p class="has-xl-font-size">Reports to: Executive Director<br>Location: Trail, British Columbia<br>Type: FTE .60, Permanent<br>Salary: $89,840 – 99,840 depending on education and experience</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:group {"metadata":{"categories":["lowercolumbia"],"patternName":"bloktastik/pink-dome","name":"Pink Dome"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"bottom":"var:preset|spacing|70","top":"0"}}},"backgroundColor":"blush","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-blush-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:cover {"url":"/wp-content/uploads/2026/02/header-dome.svg","id":357,"dimRatio":0,"overlayColor":"primary","isUserOverlayColor":false,"focalPoint":{"x":0.5,"y":0.84},"minHeight":65,"sizeSlug":"large","align":"full","className":"blush-dome","style":{"spacing":{"margin":{"top":"0"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-cover alignfull blush-dome" style="margin-top:0;min-height:65px"><img class="wp-block-cover__image-background wp-image-357 size-large" alt="" src="/wp-content/uploads/2026/02/header-dome.svg" style="object-position:50% 84%" data-object-fit="cover" data-object-position="50% 84%"/><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
-<p class="has-text-align-center has-large-font-size"></p>
-<!-- /wp:paragraph --></div></div>
-<!-- /wp:cover -->
-
-<!-- wp:block {"ref":549} /-->
+<!-- wp:group {"metadata":{"categories":["lowercolumbia"],"patternName":"bloktastik/pink-dome"},"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"bottom":"var:preset|spacing|70","top":"0"}}},"backgroundColor":"blush","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-blush-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:block {"ref":549} /-->
 
 <!-- wp:heading {"level":3,"className":"has-teal-color","style":{"elements":{"link":{"color":{"text":"var:preset|color|teal-dark"}}}},"textColor":"teal-dark","fontSize":"3xl"} -->
 <h3 class="wp-block-heading has-teal-color has-teal-dark-color has-text-color has-link-color has-3-xl-font-size"><strong>Your Role</strong></h3>
@@ -89,4 +83,4 @@
 
 <!-- wp:block {"ref":550} /--></div>
 <!-- /wp:group -->
-  
+ 
